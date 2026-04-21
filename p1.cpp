2b91178d00687e85ac90a12f22428271c64d82fc
Razfar14 <rajarangkuty14@gmail.com>
2026-04-21 15:04:35 +0700

    memulai

--- p1.cpp
+++ p1.cpp
@@ -1,16 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa{
-    public: 
-    int nim;
-    string nama;
-    float nilai;
-
-    void printData(){
-        cout << "NIM: " << nim << endl;
-        cout << "Nama: " << nama << endl;
-        cout << "Nilai: " << nilai << endl;
-    }
-
-};
